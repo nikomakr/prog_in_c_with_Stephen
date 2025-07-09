58a94f6e35d63a3646a551ddee0c54528c2a72bd
makridis nikolas <makridisnikolas@gmail.com>
2025-07-09 17:36:36 +0100

    ex02

---
+++ chapter1/ex02/ex02.c
@@ -0,0 +1,19 @@
+/* Chapter3 Ex02 Write a program, where it prints out "In C, lowercase letters are significant." newline "main is where program execution begins." newline "Opening and closing braces enclose program statements in a routine." newline "All program statements must be terminated by a semicolon." newline */
+
+#include <stdio.h>
+
+int main (void)
+{
+    printf("In C, lowercase letters are significant.\n");
+    printf("main is where program execution begins.\n");
+    printf("Opening and closing braces enclose program statements in a routine.\n");
+    printf("All program statements must be terminated by a semicolon.\n");
+
+    // Alternatively can re-written:
+    /* printf("In C, lowercase letters are significant.\n"
+           "main is where program execution begins.\n"
+           "Opening and closing braces enclose program statements in a routine.\n"
+           "All program statements must be terminated by a semicolon.\n");
+    */
+	return 0;
+}
