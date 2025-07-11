abb0dc6276f691e6ed202d3351427adb4e9b7a9f
makridis nikolas <makridisnikolas@gmail.com>
2025-07-11 15:32:02 +0100

    ex02.c

---
+++ chapter2/ex02/ex02.c
@@ -0,0 +1,26 @@
+/* Chapter2 Ex02 Which of the following are invalid variable names? Why?*/
+
+/* Int
+Invalid: ‘Int’ is a reserved keyword (case-insensitive in C/C++). */
+/* char
+Invalid: ‘char’ is a reserved keyword. */
+/* 6_05
+Invalid: Cannot start a variable name with a digit. */
+/* Calloc
+Valid */
+/* Xx
+Valid */
+/* alpha_beta_routine
+Valid */
+/* floating
+Valid */
+/* _1312
+Valid */
+/* z
+Valid */
+/* ReInitialize
+Valid */
+/* -
+Invalid: ‘-’ is not a valid identifier; it’s an operator. */
+/* A$
+Invalid: ‘$’ is not allowed in variable names in C/C++. */
