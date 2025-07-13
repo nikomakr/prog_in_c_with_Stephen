da9f1e2afc6e7fe9091b66f9a66280a7f64661be
makridis nikolas <makridisnikolas@gmail.com>
2025-07-13 16:26:52 +0100

    ex01_52.c

---
+++ chapter5/ex01/ex01_52.c
@@ -0,0 +1,23 @@
+/* Chapter5 Ex01 Type in and run the nine programs presented in this chapter. Compare the output produced each program with the output presented after each program in the text. */
+/* 5.2 Calculating the 200th triangular number */
+
+/* looping with for statement */
+
+#include <stdio.h>
+
+int main (void)
+{
+    int n, triangularNumber;
+
+    triangularNumber = 0;
+    
+    for ( n = 1; n <= 200; n = n + 1)
+        triangularNumber = triangularNumber + n;
+
+    printf("The 200th triangular number is %i\n", triangularNumber);
+
+    return 0;
+}
+
+/* OUTCOME */
+/* The 200th triangular number is 20100 */
