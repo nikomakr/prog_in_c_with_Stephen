ba0790a9244752352222dbfe4ea71b8c39aebb36
makridis nikolas <makridisnikolas@gmail.com>
2025-07-15 14:50:42 +0100

    ex01_54.c and local compiled file outcome.

---
+++ chapter5/ex01/ex01_54.c
@@ -0,0 +1,29 @@
+/* Chapter5 Ex01 Type in and run the nine programs presented in this chapter. Compare the output produced each program with the output presented after each program in the text. */
+/* 5.4 Asking a user for an input*/
+
+#include <stdio.h>
+
+int main (void)
+{
+    int n, number, triangularNumber;
+    
+    printf ("What triangular number do you want? ");
+    scanf ("%i", &number); /* The scanf is a function, which that reads and parses text from standard input. */
+
+    triangularNumber = 0;
+    
+    for ( n = 1; n <= number; ++n )
+        triangularNumber += n;
+    printf("Triangular number %i is %i\n", number, triangularNumber);
+
+    return 0;
+}
+
+/* COMMENT */
+/* Instead of using an online compiler like codechef.com I chose this time to compile it on VS Code.*/
+/* On embedded terminal type the following: */
+/* gcc -o ex01_54 ex01_54.c */
+/* It runs the first printf and requesting you to give an number. */
+/* I gave it 100 as the book example. */
+/* It runs the for and the printf by giving you: */
+/* Triangular number 100 is 5050 */
