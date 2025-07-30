0067c3cdd50edd3801a7d0107056383dcb7ef864
makridis nikolas <makridisnikolas@gmail.com>
2025-07-30 18:52:35 +0100

    ex01_71.c Working with an Array + comments

--- chapter7/ex01/ex01_71.c
+++ chapter7/ex01/ex01_71.c
@@ -1,6 +1,6 @@
 /* Chapter7 EX01 Type in and run the eight programs presented in this chapter. Compare the output produced by each program with the output presented after each program in the text. */
 
-/* Working with an Array */
+/* 7.1 Working with an Array */
 
 #include <stdio.h>
 
@@ -37,3 +37,6 @@ int main (void)
 
 /* Comment */
 // Depending on how you will compile this program, such as locally with gcc or on codechef.com or on pythontutor.com it will affect the outcome for the undefined positions of the Array, which we did not place any values. So, those remain uninitialized. They contain whatever happened to be in that memory location previously, often called "garbage values" as a result reading or using these uninitialised values is undefined behaviour.
+
+// We know by default we need an Array which can hold 10 elements, indexed from 0 to 9. That's the reason for values[10]. The very first as in any Array will be hold in values[0] and the tenth on values[9]. The values[10] it is considered out of bounds!.
+// In other words, the size inside the brackets is not the highest index, but the count of total elements.
